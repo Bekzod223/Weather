adb9de814bf6166d4e9d0cacfdb3f7d8dd7454be
Bekzod <141547940+Bekzod223@users.noreply.github.com>
2024-03-10 10:07:51 +0500

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ search.addEventListener("click", function () {
       }
 
       temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-      description.innerHTML = `${json.weather[0].description}`;
+      description.innerHTML = `${json.weather[0].description / 35}`;
       humidity.innerHTML = `${json.main.humidity}%`;
       wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     });
